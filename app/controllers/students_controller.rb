d826a31bea4c81afca339476a1295e9a6abaff8b
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-30 04:14:48 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,9 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(first_name: params[:first_name]), last_name: params[:last_name]
+    @student = Student.new 
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
     @student.save 
     #redirect to the new resource's show page
     redirect_to student_path(@student)
