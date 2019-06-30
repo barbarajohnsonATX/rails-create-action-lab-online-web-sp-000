270a8d241566fe26df40ce8701b41b86338dcb35
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-30 04:12:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,9 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new()
+    @student = Student.new(first_name: params[:first_name]), last_name: params[:last_name]
+    @student.save 
+    
   end
 
 end
